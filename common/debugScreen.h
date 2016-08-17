bfeecf9e77fd69ec90448ce84ae12795d26edbfb
yne <yne@users.noreply.github.com>
2016-08-17 23:37:04 +0200

    add VT100 support and demo

--- common/debugScreen.h
+++ common/debugScreen.h
@@ -6,7 +6,6 @@
 #include <stdarg.h>
 #include <inttypes.h>
 
-#define SCE_DISPLAY_UPDATETIMING_NEXTVSYNC SCE_DISPLAY_SETBUF_NEXTFRAME
 #include <psp2/display.h>
 #include <psp2/kernel/sysmem.h>
 #include <psp2/kernel/threadmgr.h>
@@ -18,60 +17,28 @@
 #define SCREEN_FB_WIDTH (960)
 #define SCREEN_FB_SIZE  (2 * 1024 * 1024)
 #define SCREEN_FB_ALIGN (256 * 1024)
+#define SCREEN_GLYPH_W  (8)
+#define SCREEN_GLYPH_H  (8)
 
-#define COLOR_BLACK   0xFF000000
-#define COLOR_RED     0xFF0000FF
-#define COLOR_BLUE    0xFF00FF00
-#define COLOR_YELLOW  0xFF00FFFF
-#define COLOR_GREEN   0xFFFF0000
-#define COLOR_MAGENTA 0xFFFF00FF
-#define COLOR_CYAN    0xFFFFFF00
-#define COLOR_WHITE   0xFFFFFFFF
-#define COLOR_GREY    0xFF808080
+#define COLOR_BLACK      0xFF000000
+#define COLOR_RED        0xFF0000FF
+#define COLOR_BLUE       0xFF00FF00
+#define COLOR_YELLOW     0xFF00FFFF
+#define COLOR_GREEN      0xFFFF0000
+#define COLOR_MAGENTA    0xFFFF00FF
+#define COLOR_CYAN       0xFFFFFF00
+#define COLOR_WHITE      0xFFFFFFFF
+#define COLOR_GREY       0xFF808080
+#define COLOR_DEFAULT_FG COLOR_WHITE
+#define COLOR_DEFAULT_BG COLOR_BLACK
 
-static uint32_t* psvDebugScreenBase;
+static int psvDebugScreenMutex; /*< avoid race condition when outputing strings */
 static uint32_t psvDebugScreenCoordX = 0;
 static uint32_t psvDebugScreenCoordY = 0;
-static uint32_t psvDebugScreenColorFg;
-static uint32_t psvDebugScreenColorBg;
-static int psvDebugScreenMutex; /*< avoid race condition when outputing strings */
-
-void psvDebugScreenInit() {
-	psvDebugScreenMutex = sceKernelCreateMutex("log_mutex", 0, 0, NULL);
-
-	SceKernelAllocMemBlockOpt opt = { 0 };
-	#ifdef VITASDK
-	opt.size = 4 * 5;
-	#else
-	opt.size = sizeof(opt);
-	#endif
-	opt.attr = 0x00000004;
-	opt.alignment = SCREEN_FB_ALIGN;
-	SceUID displayblock = sceKernelAllocMemBlock("display", SCE_KERNEL_MEMBLOCK_TYPE_USER_CDRAM_RW, SCREEN_FB_SIZE, &opt);
-	sceKernelGetMemBlockBase(displayblock, (void**)&psvDebugScreenBase);
-
-	SceDisplayFrameBuf framebuf = {
-		.size = sizeof(framebuf),
-		.base = psvDebugScreenBase,
-		.pitch = SCREEN_WIDTH,
-		.pixelformat = SCE_DISPLAY_PIXELFORMAT_A8B8G8R8,
-		.width = SCREEN_WIDTH,
-		.height = SCREEN_HEIGHT,
-	};
-
-	int ret = sceDisplaySetFrameBuf(&framebuf, SCE_DISPLAY_UPDATETIMING_NEXTVSYNC);
-
-	psvDebugScreenColorFg = 0xFFFFFFFF;
-	psvDebugScreenColorBg = 0x00000000;
-}
-
-void psvDebugScreenClear(int bg_color){
-	psvDebugScreenCoordX = psvDebugScreenCoordY = 0;
-	int i;
-	for(i = 0; i < SCREEN_WIDTH * SCREEN_HEIGHT; i++) {
-		psvDebugScreenBase[i] = bg_color;
-	}
-}
+static uint32_t psvDebugScreenColorFg = COLOR_DEFAULT_FG;
+static uint32_t psvDebugScreenColorBg = COLOR_DEFAULT_BG;
+static SceDisplayFrameBuf psvDebugScreenFrameBuf = {
+		sizeof(SceDisplayFrameBuf), NULL, SCREEN_WIDTH, 0, SCREEN_WIDTH, SCREEN_HEIGHT};
 
 uint32_t psvDebugScreenSetFgColor(uint32_t color) {
 	uint32_t prev_color = psvDebugScreenColorFg;
@@ -85,12 +52,58 @@ uint32_t psvDebugScreenSetBgColor(uint32_t color) {
 	return prev_color;
 }
 
-void psvDebugScreenSetCoordX(uint32_t x) {
-	psvDebugScreenCoordX = x;
+static size_t psvDebugScreenEscape(const char *str){
+	int i,j, p=0, params[8]={};
+	for(i=0; i<8 && str[i]!='\0'; i++){
+		if(str[i] >= '0' && str[i] <= '9'){
+			params[p]=(params[p]*10) + (str[i] - '0');
+		}else if(str[i] == ';'){
+			p++;
+		}else if(str[i] == 'f' || str[i] == 'H'){
+			psvDebugScreenCoordX = params[0] * SCREEN_GLYPH_W;
+			psvDebugScreenCoordY = params[1] * SCREEN_GLYPH_H;
+			break;
+		}else if (str[i] == 'm'){
+			for(j=0; j<=p; j++){
+				switch(params[j]/10){/*bold,dim,underline,blink,invert,hidden => unsupported yet */
+				#define BIT2BYTE(bit)    ( ((!!(bit&4))<<23) | ((!!(bit&2))<<15) | ((!!(bit&1))<<7) )
+				case  0:psvDebugScreenSetFgColor(COLOR_DEFAULT_FG);psvDebugScreenSetBgColor(COLOR_DEFAULT_BG);break;
+				case  3:psvDebugScreenSetFgColor(BIT2BYTE(params[j]%10));break;
+				case  9:psvDebugScreenSetFgColor(BIT2BYTE(params[j]%10) | 0x7F7F7F7F);break;
+				case  4:psvDebugScreenSetBgColor(BIT2BYTE(params[j]%10));break;
+				case 10:psvDebugScreenSetBgColor(BIT2BYTE(params[j]%10) | 0x7F7F7F7F);break;
+				#undef BIT2BYTE
+				}
+			}
+			break;
+		}
+	}
+	return i;
 }
 
-void psvDebugScreenSetCoordY(uint32_t y) {
-	psvDebugScreenCoordY = y;
+int psvDebugScreenInit() {
+	psvDebugScreenMutex = sceKernelCreateMutex("log_mutex", 0, 0, NULL);
+	SceUID displayblock = sceKernelAllocMemBlock("display", SCE_KERNEL_MEMBLOCK_TYPE_USER_CDRAM_RW, SCREEN_FB_SIZE, NULL);
+	sceKernelGetMemBlockBase(displayblock, (void**)&psvDebugScreenFrameBuf.base);
+
+	SceDisplayFrameBuf framebuf = {
+		.size = sizeof(framebuf),
+		.base = psvDebugScreenFrameBuf.base,
+		.pitch = SCREEN_WIDTH,
+		.pixelformat = SCE_DISPLAY_PIXELFORMAT_A8B8G8R8,
+		.width = SCREEN_WIDTH,
+		.height = SCREEN_HEIGHT,
+	};
+
+	return sceDisplaySetFrameBuf(&framebuf, SCE_DISPLAY_SETBUF_NEXTFRAME);
+}
+
+void psvDebugScreenClear(int bg_color){
+	psvDebugScreenCoordX = psvDebugScreenCoordY = 0;
+	int i;
+	for(i = 0; i < SCREEN_WIDTH * SCREEN_HEIGHT; i++) {
+		((uint32_t*)psvDebugScreenFrameBuf.base)[i] = bg_color;
+	}
 }
 
 int psvDebugScreenPuts(const char * text){
@@ -103,35 +116,37 @@ int psvDebugScreenPuts(const char * text){
 
 	for (c = 0; text[c] != '\0' ; c++) {
 		if (psvDebugScreenCoordX + 8 > SCREEN_WIDTH) {
-			psvDebugScreenCoordY += 8;
+			psvDebugScreenCoordY += SCREEN_GLYPH_H;
 			psvDebugScreenCoordX = 0;
 		}
 		if (psvDebugScreenCoordY + 8 > SCREEN_HEIGHT) {
 			psvDebugScreenClear(psvDebugScreenColorBg);
 		}
-		char ch = text[c];
-		if (ch == '\n') {
+		if (text[c] == '\n') {
 			psvDebugScreenCoordX = 0;
-			psvDebugScreenCoordY += 8;
+			psvDebugScreenCoordY += SCREEN_GLYPH_H;
 			continue;
-		} else if (ch == '\r') {
+		} else if (text[c] == '\r') {
 			psvDebugScreenCoordX = 0;
+			continue;
+		} else if ((text[c] == '\e') && (text[c+1] == '[')) { /* escape code (change color, position ...) */
+			c+=psvDebugScreenEscape(text+2)+2;
 			continue;
 		}
 
-		vram = psvDebugScreenBase + psvDebugScreenCoordX + psvDebugScreenCoordY * SCREEN_FB_WIDTH;
+		vram = ((uint32_t*)psvDebugScreenFrameBuf.base) + psvDebugScreenCoordX + psvDebugScreenCoordY * SCREEN_FB_WIDTH;
 
-		font = &psvDebugScreenFont[ (int)ch * 8];
-		for (i = l = 0; i < 8; i++, l += 8, font++) {
+		font = &psvDebugScreenFont[ (int)text[c] * 8];
+		for (i = l = 0; i < SCREEN_GLYPH_W; i++, l += SCREEN_GLYPH_W, font++) {
 			vram_ptr  = vram;
-			for (j = 0; j < 8; j++) {
+			for (j = 0; j < SCREEN_GLYPH_W; j++) {
 				if ((*font & (128 >> j))) *vram_ptr = psvDebugScreenColorFg;
 				else *vram_ptr = psvDebugScreenColorBg;
 				vram_ptr++;
 			}
 			vram += SCREEN_FB_WIDTH;
 		}
-		psvDebugScreenCoordX += 8;
+		psvDebugScreenCoordX += SCREEN_GLYPH_W;
 	}
 
 	sceKernelUnlockMutex(psvDebugScreenMutex, 1);
@@ -149,6 +164,5 @@ int psvDebugScreenPrintf(const char *format, ...) {
 
 	return ret;
 }
-
 
 #endif
--- debug_print/src/main.c
+++ debug_print/src/main.c
@@ -3,10 +3,33 @@
 
 #include "debugScreen.h"
 
+#define printf psvDebugScreenPrintf
+
 int main(int argc, char *argv[]) {
+	int remain = 5;
 	psvDebugScreenInit();
-	psvDebugScreenPrintf("Hello, world!\nThis sample will close in 5s...");
-	sceKernelDelayThread(5*1000*1000);
+
+	printf("Welcome to the psvDebugScreen showcase !\n");
+	
+	/* print some bg/fg colors */
+	const char* message = "Let's have some foreground/background colors !\n";
+	int modes[]={3,9,4,10}, c;
+	for(c = 0; message[c] != '\0'; c++)
+		printf("\e[%i%im%c", modes[(c/8)%4], c%8, message[c]);
+
+	/* reset fg/bg color back to default */
+	printf("\e[m");
+
+	/* \r demo using a countdown */
+	while(remain-->0){
+		printf("This sample will close in %i s...\r", remain);
+		sceKernelDelayThread(1000*1000);
+	}
+
+	/* print at specific col;row */
+	printf("\e[10;20HBye Bye");
+	sceKernelDelayThread(2*1000*1000);
+
 	sceKernelExitProcess(0);
 	return 0;
 }
