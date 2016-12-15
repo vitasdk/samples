a7395e304c0543e13a49fb5c35873647578391a7
devnoname120 <devnoname120+gh@gmail.com>
2016-12-15 03:39:58 +0100

    Improve hello_world and add delay

--- hello_world/src/main.c
+++ hello_world/src/main.c
@@ -1,8 +1,17 @@
+#include <psp2/kernel/threadmgr.h>
 #include <psp2/kernel/processmgr.h>
 #include <stdio.h>
 
+#include "debugScreen.h"
+
+
+#define printf psvDebugScreenPrintf
+
 int main(int argc, char *argv[]) {
+	psvDebugScreenInit();
 	printf("Hello, world!\n");
+	
+	sceKernelDelayThread(3*1000000); // Wait for 3 seconds
 	sceKernelExitProcess(0);
 	return 0;
 }
