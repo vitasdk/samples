a4a744dc8a04b1beaf5c6cda1d573eafe119f52b
Ivan Epifanov <isage.dna@gmail.com>
2022-08-14 20:59:44 +0300

    Fix prx loader/plugin samples

--- prx_loader/main.c
+++ prx_loader/main.c
@@ -2,72 +2,88 @@
 #include <psp2/kernel/modulemgr.h> 
 #include <psp2/io/fcntl.h>
 #include <psp2/ctrl.h>
+#include <psp2/display.h>
 
 #include "debugScreen.h"
+
+void simple_greeting(); // plugin function
+
+// plugin parameters
+typedef int (*print_func)(const char *format, ...);
+typedef struct {
+    print_func print;
+} plugin_param;
+
 #define printf psvDebugScreenPrintf
+
 #define POS(C,L) "\033[" C ";" L "H"
+
 #define ONPRESS(flag) ((ctrl.buttons & (flag)) && !(ctrl_old.buttons & (flag)))
+
 #define prel31_to_addr(ptr) ({ \
     long offset = (((long)*(ptr)) << 1) >> 1; \
     (unsigned long)(ptr) + offset; \
 })
-int main(int argc, char *argv[]) {
-	psvDebugScreenInit();
-	//store psvDebugScreen context to be sent to the module on start
-	struct{int mutex;SceDisplayFrameBuf*fbuf;} arg = {psvDebugScreenMutex, &psvDebugScreenFrameBuf};
 
-	SceUID modid=0;
-	int status=0, ret=0;
-	//char arg_init[] = "wake\0up!";
-	char arg_fini[] = "see you!";
-	char*suprx_path = "ux0:plugin.suprx";
-	SceKernelModuleInfo mi;
-	SceCtrlData ctrl,ctrl_old={};
-	for(;;){
-		printf(POS("0","0")"PRXloader (Dpad)\n");
-		printf("Up   : Boot Up '%s'\n", suprx_path);
-		printf("Down : Shut Down with '%s' argument\n", arg_fini);
-		printf("Left : List All Modules\n");
-		printf("Right: Call PRX Greeting function\n\n");
-		printf("ModuleId  :%08X\n", modid);
-		printf("start/stop:%08X\n", status);
-		printf("last call :%08X\n", ret);
-		ctrl_old = ctrl;
-		sceCtrlReadBufferPositive(0, &ctrl, 1);
-		if(ONPRESS(SCE_CTRL_UP)){
-			modid = sceKernelLoadStartModule(suprx_path, sizeof(arg), &arg, 0, NULL, &status);
-		}
-		if(ONPRESS(SCE_CTRL_DOWN)){
-			ret = sceKernelStopUnloadModule(modid, sizeof(arg_fini), arg_fini, 0, NULL, &status);
-			modid = 0;
-		}
-		if(ONPRESS(SCE_CTRL_LEFT)){
-			SceUID modids[32];//typycally ~13 are returned
-			int num = sizeof(modids)/sizeof(*modids);
-			ret = sceKernelGetModuleList(0x80|0x1, modids, &num);// 0/1 = user 0x80=system
-			psvDebugScreenClear(0);
-			printf(POS("0","10")"ModuleList:\n");
-			printf("---ID--- ------name-----\n");
-			for(int n = 0; n < num; n++){
-				ret = sceKernelGetModuleInfo(modids[n],&mi);/* TODO: segments[4] ? */
-				printf("%08lX <%X-%lX> [%s%s%s%s] %-15s %-40s %4i %4i %8p\n", mi.modid, sizeof(mi), mi.size,
-				       mi.unk28?"i":" ", mi.module_start?"s":" ", mi.module_stop?"s":" ", mi.module_exit?"e":" ",
-				       mi.module_name, mi.path,  mi.extabBtm - mi.extabTop, mi.exidxBtm - mi.exidxTop, mi.tlsInit);
-				//Advanced informations: section/exception unwinding
-				//for(SceKernelModuleExtab*i = mi.importTop; i < mi.importBtm; i++)
-				//	printf("%p ", i->insn0, i->insn1);
-				//printf("\n");
-				//for(SceKernelModuleExidx*i = mi.exidxTop; i < mi.exidxBtm; i++)
-				//	printf("%08lX ", prel31_to_addr(&i->ptr));
-				//printf("\n");
-			}
-		}
-		if(ONPRESS(SCE_CTRL_RIGHT) && modid){
-			ret = sceKernelGetModuleInfo(modid,&mi);
-			if(!ret && mi.module_stop)
-				status = mi.module_stop(0,"but not from ModuleMgr");
-		}
-	}
-	sceKernelExitProcess(0);
-	return 0;
+int main(int argc, char *argv[]) {
+    psvDebugScreenInit();
+
+    SceUID modid=0;
+    int status=0, ret=0;
+
+    // pass printf function pointer to plugin
+    plugin_param args;
+    args.print = printf;
+
+    char arg_fini[] = "see you!";
+    char* suprx_path = "ux0:/data/prx_simple.suprx";
+    SceKernelModuleInfo mi;
+    SceCtrlData ctrl, ctrl_old={};
+
+    for(;;){
+        printf(POS("0","0")"PRXloader (Dpad)\n");
+        printf("Up   : Boot Up '%s'\n", suprx_path);
+        printf("Down : Shut Down with '%s' argument\n", arg_fini);
+        printf("Left : List All Modules\n");
+        printf("Right: Call PRX Greeting function\n\n");
+        printf("ModuleId  :%08X\n", modid);
+        printf("start/stop:%08X\n", status);
+        printf("last call :%08X\n", ret);
+        ctrl_old = ctrl;
+        sceCtrlReadBufferPositive(0, &ctrl, 1);
+        if(ONPRESS(SCE_CTRL_UP)){
+            modid = sceKernelLoadStartModule(suprx_path, sizeof(args), &args, 0, NULL, &status);
+        }
+        if(ONPRESS(SCE_CTRL_DOWN)){
+            ret = sceKernelStopUnloadModule(modid, sizeof(arg_fini), arg_fini, 0, NULL, &status);
+            modid = 0;
+        }
+        if(ONPRESS(SCE_CTRL_LEFT)){
+            SceUID modids[32];//typycally ~13 are returned
+            SceSize num = sizeof(modids)/sizeof(*modids);
+            ret = sceKernelGetModuleList(0x80|0x1, modids, &num);// 0/1 = user 0x80=system
+            psvDebugScreenClear(0);
+            printf(POS("10","0")"ModuleList:\n");
+            printf("---ID--- ------name-----\n");
+            for(int n = 0; n < num; n++){
+                ret = sceKernelGetModuleInfo(modids[n],&mi);/* TODO: segments[4] ? */
+                printf("%08lX <%X-%lX> [%s%s%s%s] %-15s %-40s %4i %4i %8p\n", mi.modid, sizeof(mi), mi.size,
+                       mi.unk28?"i":" ", mi.start_entry?"s":" ", mi.stop_entry?"s":" ", mi.exit_entry?"e":" ",
+                       mi.module_name, mi.path,  mi.extab_btm - mi.extab_top, mi.exidx_btm - mi.exidx_top, mi.tlsInitSize);
+
+                //Advanced informations: section/exception unwinding
+                //for(SceKernelModuleExtab*i = mi.importTop; i < mi.importBtm; i++)
+                //    printf("%p ", i->insn0, i->insn1);
+                //printf("\n");
+                //for(SceKernelModuleExidx*i = mi.exidxTop; i < mi.exidxBtm; i++)
+                //    printf("%08lX ", prel31_to_addr(&i->ptr));
+                //printf("\n");
+            }
+        }
+        if(ONPRESS(SCE_CTRL_RIGHT) && modid){
+            simple_greeting();
+        }
+    }
+    sceKernelExitProcess(0);
+    return 0;
 }
--- prx_simple/main.c
+++ prx_simple/main.c
@@ -1,37 +1,39 @@
 #include <psp2/kernel/clib.h>
-#include <psp2/io/fcntl.h>
-#include <psp2/kernel/sysmem.h>
 #include <psp2/kernel/modulemgr.h>
-#include <psp2/kernel/threadmgr.h>
-#include <psp2/sysmodule.h>
-#include <psp2/net/net.h>
-#include <psp2/net/netctl.h>
+#include <string.h>
 
-#include "debugScreen.h"
-#define printf psvDebugScreenPrintf
 #define POS(C,L) "\033[" C ";" L "H"
 
-int simple_greeting(int in){
-	return in*2;
+// plugin parameters passed by main executable
+typedef int (*print_func)(const char *format, ...);
+typedef struct {
+    print_func print;
+} plugin_param;
+
+static plugin_param params;
+
+void simple_greeting(){
+    params.print(POS("61","1")"Sup!\n");
 }
 
-int module_stop(SceSize argc, const void *args){
-	printf(POS("64","2")"I, PRX, have been stopped (%s)\n",(char*)args);
-	return SCE_KERNEL_STOP_SUCCESS;
+int module_stop(SceSize argc, void *args) {
+    params.print(POS("60","1")"I, PRX, have been stopped (%s)\n",(char*)args);
+    return SCE_KERNEL_STOP_SUCCESS;
 }
 
 int module_exit(){
-	printf(POS("64","3")"Blarg\n");
-	return SCE_KERNEL_STOP_SUCCESS;
-}
-void _start() __attribute__ ((weak, alias ("module_start")));
-int module_start(SceSize argc, void *args){
-	// The loader gave us SceDisplay info so we can print
-	struct{int mutex;SceDisplayFrameBuf*fbuf;}*in = args;
-	psvDebugScreenMutex = in->mutex;
-	psvDebugScreenFrameBuf = *in->fbuf;
-	printf(POS("64","1")"Hello world from PRX!\n");
-	printf(POS("64","2")"start:%p stop:%p exit:%p func:%p\n",module_start, module_stop, module_exit, simple_greeting);
-	return SCE_KERNEL_START_SUCCESS;
+    params.print(POS("61","1")"Blarg\n");
+    return SCE_KERNEL_STOP_SUCCESS;
 }
 
+void _start() __attribute__ ((weak, alias ("module_start")));
+int module_start(SceSize argc, void *args) {
+    sceClibPrintf("Sup!\n");
+
+    params.print = ((plugin_param*)args)->print; // get pointer to print func
+
+    params.print(POS("63","1")"Hello world from PRX!\n");
+    params.print(POS("64","1")"start:%p stop:%p exit:%p func:%p\n", module_start, module_stop, module_exit, simple_greeting);
+
+    return SCE_KERNEL_START_SUCCESS;
+}
