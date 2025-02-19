b218d826f567bb8636d394121ed1c1f2ae3cd666
MrcSnm <msnmancini@hotmail.com>
2025-02-19 07:44:39 -0300

    Added: Minimal message box example

---
+++ message_box/src/gxm_base.h
@@ -0,0 +1,10 @@
+#ifndef GXM_BASE_H
+#define GXM_BASE_H
+
+void gxm_minimal_init_for_common_dialog(void);
+void gxm_init_for_common_dialog(void);
+void gxm_swap_for_common_dialog(void);
+void gxm_term_for_common_dialog(void);
+void gxm_minimal_term_for_common_dialog(void);
+
+#endif
