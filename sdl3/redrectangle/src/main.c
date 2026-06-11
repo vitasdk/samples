9155a7352c50147656f2e03904ccb0f2b394b6d4
Fancy2209 <64917206+Fancy2209@users.noreply.github.com>
2026-06-11 13:28:58 +0000

    Change SDL3 Sample to use Callbacks

--- sdl3/redrectangle/src/main.c
+++ sdl3/redrectangle/src/main.c
@@ -1,5 +1,7 @@
 #include <psp2/kernel/processmgr.h>
+#define SDL_MAIN_USE_CALLBACKS 1  /* use the callbacks instead of main() */
 #include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
 
 //Screen dimension constants
 enum {
@@ -16,28 +18,43 @@ SDL_FRect fillRect = { SCREEN_WIDTH  / 4,
 		      SCREEN_HEIGHT / 2
 };
 
-
-
-int main(int argc, char *argv[])
+/* This function runs once at startup. */
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) 
 {
   if( !SDL_Init( SDL_INIT_VIDEO ) )
-      return -1;
+    return SDL_APP_FAILURE;
 
   if ((gWindow = SDL_CreateWindow( "RedRectangle", SCREEN_WIDTH, SCREEN_HEIGHT, 0)) == NULL)
-    return -1;
-
+    return SDL_APP_FAILURE;
+  
   if ((gRenderer = SDL_CreateRenderer( gWindow, 0)) == NULL)
-      return -1;
+      return SDL_APP_FAILURE;
 
+    return SDL_APP_CONTINUE;  /* carry on with the program! */
+}
+
+/* This function runs when a new event (mouse input, keypresses, etc) occurs. */
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
+{
+    if (event->type == SDL_EVENT_QUIT) {
+        return SDL_APP_SUCCESS;  /* end the program, reporting success to the OS. */
+    }
+    return SDL_APP_CONTINUE;  /* carry on with the program! */
+}
+
+/* This function runs once per frame, and is the heart of the program. */
+SDL_AppResult SDL_AppIterate(void *appstate)
+{
   SDL_SetRenderDrawColor( gRenderer, 255,0,0,255);
   SDL_RenderFillRect( gRenderer, &fillRect );
   SDL_RenderPresent( gRenderer );
-  SDL_Delay(4000);
-  SDL_DestroyRenderer( gRenderer );
-  SDL_DestroyWindow( gWindow );
+  return SDL_APP_CONTINUE;  /* carry on with the program! */
+}
+
+/* This function runs once at shutdown. */
+void SDL_AppQuit(void *appstate, SDL_AppResult result)
+{
+  /* SDL will clean up the window/renderer for us. */
   gWindow = NULL;
   gRenderer = NULL;
-
-  SDL_Quit();
-  return 0;
 }
